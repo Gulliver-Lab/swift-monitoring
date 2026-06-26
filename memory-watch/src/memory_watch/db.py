bb6f05817ce623b31a49d560705ac4a4c23913ee
François Villemot <francois.villemot@espci.fr>
2026-06-26 14:00:10 +0200

    fix: queries...

--- memory-watch/src/memory_watch/db.py
+++ memory-watch/src/memory_watch/db.py
@@ -14,22 +14,24 @@ def load_jobs_for_period(start: datetime, end: datetime) -> pd.DataFrame:
         conn = mariadb.connect(**db_config())
         query = f"""
         SELECT
-            j.job_id,
-            j.user,
+            j.id_job,
+            u.name AS `user`,
             j.job_name,
-            j.started_at,
-            j.ended_at,
+            j.time_start,
+            j.time_end,
             j.mem_req,
             j.tres_req,
             s.tres_usage_in_max,
             s.tres_usage_in_max_nodeid,
             s.tres_usage_in_max_taskid
         FROM default_job_table AS j
+        INNER JOIN user_table AS u
+            ON j.id_user = u.id_user
         INNER JOIN default_step_table AS s
             ON j.job_db_inx = s.job_db_inx
            AND s.id_step = -5
-        WHERE j.ended_at > {start.timestamp()}
-          AND j.ended_at <= {end.timestamp()}
+        WHERE j.time_end > {start.timestamp()}
+          AND j.time_end <= {end.timestamp()}
         """
         return pd.read_sql_query(query, con=conn)
     finally:
