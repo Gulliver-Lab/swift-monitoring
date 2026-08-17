7c5e474770f632053f98a62ebe2f8a6096577edb
François Villemot <francois.villemot@espci.fr>
2026-08-17 13:42:05 +0200

    feat: model for users

--- zfs-watch/src/zfs_watch/cli.py
+++ zfs-watch/src/zfs_watch/cli.py
@@ -1,2 +1,17 @@
+import dataclasses
+
+
+@dataclasses.dataclass
+class User:
+    uid: int
+    name: str | None
+    used: str
+    pi: str | None
+
+
+def get_expired_users() -> list[User]:
+    pass
+
+
 def main() -> None:
     print("track expired")
