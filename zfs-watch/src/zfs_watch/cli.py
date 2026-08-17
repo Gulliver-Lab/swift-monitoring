126fbcf6c33cc80c78c17f9ea1d3f3364e94e42e
François Villemot <francois.villemot@espci.fr>
2026-08-17 13:51:08 +0200

    feat: get expired list (#6)
    Get the list of people with expired accounts, and the disk space they
    use.
    
    Currently uses a placeholder command for dev.

--- zfs-watch/src/zfs_watch/cli.py
+++ zfs-watch/src/zfs_watch/cli.py
@@ -1,2 +1,31 @@
+import dataclasses
+import subprocess
+
+
+@dataclasses.dataclass
+class User:
+    uid: int
+    name: str | None
+    used: str
+    pi: str | None
+
+
+def get_expired_users() -> list[User]:
+    # TODO: change the call to zfs userspace gulliver/home
+    result = subprocess.run(
+        ["cat", "/home/francois/tmp/zfs.txt"], stdout=subprocess.PIPE
+    ).stdout.decode("utf-8")
+
+    users = []
+    for line in result.splitlines()[1:]:
+        _, _, uid, used, _, _, _ = line.split()
+        # Only keep the ones where the "name" is the uid
+        # These are the expired accounts
+        if uid.isdigit():
+            users.append(User(int(uid), None, used, None))
+
+    return users
+
+
 def main() -> None:
     print("track expired")
